3584cde070b10137966af03c7ec040e470dc8b7f
suudi55 <suudituna55@gmail.com>
2024-06-16 21:51:50 +0300

    Add files via upload

---
+++ cars.py
@@ -0,0 +1,37 @@
+cars=['bmw','toyota']
+for car in cars:
+    if car=='bmw':
+        print(car.title())
+requested_toppings=['mushrooms','onions','pineapple']
+print('mushrooms'in requested_toppings)
+banned_users=['andrew','carolina']
+user='marie'
+if user not in banned_users:
+    print(user.title()+",you can post response if you wish.")
+    
+              
+              
+car='subaru'
+print("Is car=='subaru'?")
+print(car=='subaru')
+print(car.upper()=='subaru')
+age=12
+if age<4:
+    price=0
+if age<18:
+    price=5
+else:
+    price=10
+print(" your admission cost is $"+ str(price)+".")
+alien_color=['green','yellow','red']
+if 'green' in alien_color:
+    print("player earned 5 points.")
+if 'blue'in alien_color:
+    print('hello')
+alien_color='green'
+if alien_color=='green':
+    print("player earned 5 points.")
+if alien_color!='green':
+    print("the player earned 10 points.")
+else:
+    print("hello")
